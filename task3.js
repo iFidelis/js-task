dde2fec361677fcc1be404891678b2aa5c7cabc9
Fidelis Inaku <f.inaku@tropicdreams.com.ng>
2020-04-16 17:18:37 +0100

    Update task3.js

--- task3.js
+++ task3.js
@@ -3,8 +3,7 @@ let divide = (num) => {
     let arr = [];
 
     for(let i = 1; i <= num; i++){
-        if(i % 2 === 0) {
-            if (i % 3 == 0) {
+            if (i % 3 === 0) {
                 arr.push("yu-gi");
             }
             else if (i % 5 === 0) {
@@ -13,10 +12,12 @@ let divide = (num) => {
             else if (i % 3 === 0 && i % 5 === 0){
                 arr.push("yu-gi-oh");
             }
-            else {
+            else if (i % 2 === 0){
                 arr.push("yu");
             }
-        }
+            else {
+                arr.push(i);
+            }
     }
     return arr;
     }
